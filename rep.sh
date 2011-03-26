ec0510f49983edeb844f8846fa50e25249743b73
Krzysiek <krzysiula@o2.pl>
2011-03-26 19:19:42 +0100

    Dodanie pliku basha z wydawanymi komendami

---
+++ rep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git init	# nowe repozytorium lokalnie
+
+git add main.cpp	# main.cpp będzie śledzony w repo
+
+git commit -m "Rozpoczęcie projektu"	# pierwszy commit
+
+
+git add rep.sh	# dodajemy następny plik do śledzenia
